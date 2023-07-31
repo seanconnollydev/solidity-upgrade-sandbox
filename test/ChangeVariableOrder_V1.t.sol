613d40c99a0f0831bbfb00afa30f221627cde75c
Sean Connolly <sean@seanconnolly.dev>
2023-07-31 12:23:21 -0600

    Create new contract

--- test/ChangeVariableOrder_V1.t.sol
+++ test/ChangeVariableOrder_V1.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {ChangeVariableOrder_V1} from "../src/ChangeVariableOrder_V1.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract ChangeVariableOrder_V1_Test is Test {
+    ChangeVariableOrder_V1 public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new ChangeVariableOrder_V1();
         counter.setNumber(0);
     }
 
